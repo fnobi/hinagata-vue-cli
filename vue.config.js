e97640ec548f1ad93c0841c9498eb6339a7d7dfc
nobii <fujisawa-shin@kayac.com>
2018-11-01 23:23:22 +0900

    情報を増やす

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+const path = require("path");
 const { BASE_PATH, HOST_NAME, META } = require("./src/constants.json");
+
+const TEMPLATE_PARAMS = {
+    HOST_NAME,
+    META,
+    BASE_PATH,
+    BASE_URL: path.join(HOST_NAME, BASE_PATH)
+};
 
 module.exports = {
     baseUrl: BASE_PATH,
@@ -12,15 +20,16 @@ module.exports = {
     },
     chainWebpack: config => {
         config.plugin("html").tap(args => {
-            return args.map(opts => {
-                const original = opts.templateParameters;
-                opts.templateParameters = function(opts) {
-                    return Object.assign({}, original(opts), {
-                        HOST_NAME,
-                        META
-                    });
-                };
-                return opts;
+            return args.map(arg => {
+                return Object.assign({}, arg, {
+                    templateParameters(params) {
+                        return Object.assign(
+                            {},
+                            arg.templateParameters(params),
+                            TEMPLATE_PARAMS
+                        );
+                    }
+                });
             });
         });
     }
